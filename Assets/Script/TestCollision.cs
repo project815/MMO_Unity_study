2de87789c4e0c5313c57d44eeb1a0e2a86c6be32
songminseok <ske3056@gmail.com>
2024-03-03 21:09:23 +0900

    feat: onTriggerEnter

--- Assets/Script/TestCollision.cs
+++ Assets/Script/TestCollision.cs
@@ -5,12 +5,14 @@ public class TestCollision : MonoBehaviour
   // 1. 나한테 rigidbody가 있어야 한다.(isKinematic : off)
   // 2. 나한테 collider가 있어야 한다. (isTrigger : off)
   // 3. 상대한테 collider가 있어야 한다. (isTrigger : off)
-  
   private void OnCollisionEnter(Collision other)
-  { 
-    Debug.Log("onCollisionEnter");
+  {
+    Debug.Log($"collision : {other.gameObject.name}");
   }
-
+  
+  // 1. 둘 다 collider가 있어야 한다.
+  // 2. 둘 중 하나는 isTrigger : on
+  // 3. 둘 중 하나는 Rigidbody가 있어야 한다.
   private void OnTriggerEnter(Collider other)
   { 
     Debug.Log("OnTriggerEnter");
