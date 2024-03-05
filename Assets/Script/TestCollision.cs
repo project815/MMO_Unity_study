cf1f90937c0ca477463e385955537f944af0c82d
songminseok <ske3056@gmail.com>
2024-03-05 19:56:48 +0900

    feat: 투영

--- Assets/Script/TestCollision.cs
+++ Assets/Script/TestCollision.cs
@@ -25,14 +25,14 @@ public class TestCollision : MonoBehaviour
     // 로컬 좌표 전환
     Vector3 look = transform.TransformDirection(Vector3.forward);
     
-    Debug.DrawRay(transform.position + Vector3.up, look * 100, Color.red);
-    // RayCastHit 값
-    RaycastHit hit;
-    if(Physics.Raycast(transform.position + Vector3.up, look * 100, out hit))
-    {
-      //출력 이름
-      Debug.Log($"RayCasting!~{hit.transform.gameObject.name}");
-    }
+    // Debug.DrawRay(transform.position + Vector3.up, look * 100, Color.red);
+    // // RayCastHit 값
+    // RaycastHit hit;
+    // if(Physics.Raycast(transform.position + Vector3.up, look * 100, out hit))
+    // {
+    //   //출력 이름
+    //   Debug.Log($"RayCasting!~{hit.transform.gameObject.name}");
+    // }
 
 
     // RaycastHit[] hit = Physics.RaycastAll(transform.position + Vector3.up, look * 100);
@@ -45,5 +45,11 @@ public class TestCollision : MonoBehaviour
     
     // 사용 예시
     // 캐릭터와 카메라 사이에 다른 물체가 있다면, 카메라를 앞쪽으로 이동하도록 만들 수 있다.
+    
+    
+    // Local <-> World <-> Veiwport <-(유사)-> Screen(화면) 
+    // Debug.Log(Input.mousePosition); // Screen 픽셀좌표
+    // Debug.Log(Camera.main.ScreenToViewportPoint(Input.mousePosition)); //Veiwport - 비율좌표 
+    // 투영 - 절두체 안에 내용물이 찍히게 됌.
   } 
 }
