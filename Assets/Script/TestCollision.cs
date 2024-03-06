886d9e4550519a507b9246821045c9733e6ff11e
songminseok <ske3056@gmail.com>
2024-03-06 23:49:42 +0900

    faet: raycast2

--- Assets/Script/TestCollision.cs
+++ Assets/Script/TestCollision.cs
@@ -1,55 +1,105 @@
-using System;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem.HID;
 
-public class TestCollision : MonoBehaviour
+public class CollisionTest : MonoBehaviour
 {
-  // 1. 나한테 rigidbody가 있어야 한다.(isKinematic : off)
-  // 2. 나한테 collider가 있어야 한다. (isTrigger : off)
-  // 3. 상대한테 collider가 있어야 한다. (isTrigger : off)
-  private void OnCollisionEnter(Collision other)
-  {
-    Debug.Log($"collision : {other.gameObject.name}");
-  }
-  
-  // 1. 둘 다 collider가 있어야 한다.
-  // 2. 둘 중 하나는 isTrigger : on
-  // 3. 둘 중 하나는 Rigidbody가 있어야 한다.
-  private void OnTriggerEnter(Collider other)
-  { 
-    Debug.Log("OnTriggerEnter");
-  }
+    /* Collision 발동 조건
+     * 1) 나한테 RigidBody가 있어야 한다 (IsKinematic : Off)
+     * 2) 나한테 Collider가 있어야 한다 (IsTrigger : Off)
+     * 3) 상대한테 Collider가 있어야 한다 (IsTrigger : Off)
+     */
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log($"Collision @ {collision.gameObject.name}!");
+    }
 
-  private void Update()
-  {
-    // 로컬 좌표 전환
-    Vector3 look = transform.TransformDirection(Vector3.forward);
-    
-    // Debug.DrawRay(transform.position + Vector3.up, look * 100, Color.red);
-    // // RayCastHit 값
-    // RaycastHit hit;
-    // if(Physics.Raycast(transform.position + Vector3.up, look * 100, out hit))
-    // {
-    //   //출력 이름
-    //   Debug.Log($"RayCasting!~{hit.transform.gameObject.name}");
-    // }
+    /* Trigger 발동 조건
+     * 1) 둘 다 Collider가 있어야 한다
+     * 2) 둘 중 하나는 IsTrigger : On
+     * 3) 둘 중 하나는 RigidBody가 있어야 한다
+     */
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log($"Trigger @ {other.gameObject.name}!");
+    }
 
+    private void Update()
+    {
+        /* 투영의 개념
+         * Local(특정 물체 좌표계) <-> World(게임 세상 좌표계) <-> Viewport <-> Screen(화면/2D)
 
-    // RaycastHit[] hit = Physics.RaycastAll(transform.position + Vector3.up, look * 100);
-    //
-    // foreach(RaycastHit i in hit)
-    // {
-    //   Debug.Log(i.transform.gameObject.name);      
-    // }
+        Debug.Log(Input.mousePosition); // Screen 좌표(px) 1980 x1080  --
+        Debug.Log(Camera.main.ScreenToViewportPoint(Input.mousePosition)); // Viewport 좌표(비율) -- 1 : 1 0.7: 0.3
+         */
+        
+        
+        if (Input.GetMouseButtonDown(0))
+        {
+            
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.nearClipPlane));
+            Debug.Log("new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.nearClipPlane :"+  mousePos);
+            Debug.Log("Camera.main.ScreenPointToRay(Input.mousePosition) : "+ Camera.main.ScreenPointToRay(Input.mousePosition));
+            // Debug.Log("Camera.main.ScreenToViewportPoint(Input.mousePosition) :  "_+ Camera.main.ScreenToViewportPoint(Input.mousePosition));
+            // Debug.Log("Camera.main.transform.position" + Camera.main.transform.position);
+            Vector3 dir = mousePos - Camera.main.transform.position;
+            dir = dir.normalized;
+            // Debug.Log(Camera.main.(Input.mousePosition)); // Viewport 좌표(비율) -- 1 : 1 0.7: 0.3
 
-    
-    // 사용 예시
-    // 캐릭터와 카메라 사이에 다른 물체가 있다면, 카메라를 앞쪽으로 이동하도록 만들 수 있다.
-    
-    
-    // Local <-> World <-> Veiwport <-(유사)-> Screen(화면) 
-    // Debug.Log(Input.mousePosition); // Screen 픽셀좌표
-    // Debug.Log(Camera.main.ScreenToViewportPoint(Input.mousePosition)); //Veiwport - 비율좌표 
-    // 투영 - 절두체 안에 내용물이 찍히게 됌.
-  } 
-}
+            /* 코드 단축
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.nearClipPlane));
+            Vector3 dir = mousePos - Camera.main.transform.position;
+            dir = dir.normalized;
+            
+            Debug.DrawRay(Camera.main.transform.position, dir * 100.0f, Color.red, 1.0f);
+
+            RaycastHit hit;
+            if (Physics.Raycast(Camera.main.transform.position, dir, out hit, 100.0f))
+            {
+                Debug.Log($"Raycast Camera @ {hit.collider.gameObject.name}");
+            }
+             */
+            /* Layer Mask
+             * Raycast는 과부화주는 작업이기에 최적화 작업 필요
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Debug.DrawRay(Camera.main.transform.position, ray.direction * 100.0f, Color.red, 1.0f);
+
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, 100.0f))
+            {
+                Debug.Log($"Raycast Camera @ {hit.collider.gameObject.name}");
+            }
+             */
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Debug.DrawRay(Camera.main.transform.position, ray.direction * 100.0f, Color.red, 1.0f);
+
+            LayerMask mask = LayerMask.GetMask("Monster") | LayerMask.GetMask("Wall");
+            // int mask = (1 << 8) | (1 << 9); // 비트 플래그
+
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, 100.0f, mask))
+            {
+                Debug.Log($"Raycast Camera @ {hit.collider.gameObject.name}");
+            }
+        }
+
+        /* Raycast Test
+        Vector3 look = transform.TransformDirection(Vector3.forward);
+        Debug.DrawRay(transform.position + Vector3.up, look * 10, Color.red);
+
+        // 단일
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position + Vector3.up, look, out hit, 10))
+        {
+            Debug.Log($"Raycast {hit.collider.gameObject.name}!");
+        }
+
+        // 복수
+        RaycastHit[] hits = Physics.RaycastAll(transform.position + Vector3.up, look, 10);
+        foreach(RaycastHit hit in hits)
+        {
+            Debug.Log($"Raycast {hit.collider.gameObject.name}!");
+        }
+        */
+    }
+    }
