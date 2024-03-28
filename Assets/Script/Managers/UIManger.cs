529d7ba5b71ac674a23d8aeff2a1b073240ab0db
songminseok <ske3056@gmail.com>
2024-03-28 19:39:26 +0900

    feat : ui Manager

--- Assets/Script/Managers/UIManger.cs
+++ Assets/Script/Managers/UIManger.cs
@@ -1,14 +1,59 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 
 public class UIManger
 {
-    private int _order = 0;
+    private int _order = 10;
 
     private Stack<UI_Popup> _popupStack = new Stack<UI_Popup>();
+    private UI_Scene _uiScene = null;
 
+    private GameObject _Root
+    {
+        get
+        {
+            GameObject root = GameObject.Find("@UI_Root");
+            if (root == null) // 하이라키 내에 @Managers가 없으면 생성한다.
+            {
+                root = new GameObject { name = "@UI_Root" };
+            }
+            return root;
+        }
+    }
 
+    public void SetCanvas(GameObject go, bool sort = true)
+    {
+        Canvas canvas = Util.GetOrAddComponent<Canvas>(go);
+        canvas.renderMode = RenderMode.ScreenSpaceCamera;
+        canvas.overrideSorting = true; 
+        if (sort)
+        {
+            canvas.sortingOrder = _order;
+            _order++;
+        }
+        else
+        {
+            canvas.sortingOrder = 0;
+        }
+    }
+
+    public T ShowSceneUI<T>(string name = null) where T : UI_Scene
+    {
+        if (string.IsNullOrEmpty(name))
+        {
+            name = typeof(T).Name;
+        }
+
+        GameObject go = Managers.Resource.Instantiate($"UI/Scene/{name}");
+        T scene = Util.GetOrAddComponent<T>(go);
+        _uiScene = scene;
+        
+        go.transform.SetParent(_Root.transform);
+
+        return scene;
+    }
     public T ShowPopupUI<T>(string name = null)where T : UI_Popup
     {
         if (string.IsNullOrEmpty(name))
@@ -17,14 +62,15 @@ public class UIManger
         }
 
         GameObject go = Managers.Resource.Instantiate($"UI/Popup/{name}");
-
         T popup = Util.GetOrAddComponent<T>(go);
-        
         _popupStack.Push(popup);
+        
+        go.transform.SetParent(_Root.transform);
+
         return popup;
     }
 
-    public void ClosePopUI(UI_Popup popup)
+    public void ClosePopupUI(UI_Popup popup)
     {
         if (_popupStack.Count == 0)
             return;
--- Assets/Script/UI/Popup/UI_Button.cs
+++ Assets/Script/UI/Popup/UI_Button.cs
@@ -35,24 +35,23 @@ public class UI_Button : UI_Popup
 
     private void Start()
     {
-        
-        Debug.Log(typeof(Texts));
+        Init();
+    }
+
+    public override void Init()
+    {
+        base.Init();
         Bind<Button>(typeof(Buttons)); //reflection ????? c# ?? 
         Bind<Text>(typeof(Texts));
         Bind<Image>(typeof(Images));
 
         Bind<GameObject>(typeof(GameObjects));
-        
-        
         GetText((int)Texts.ScoreText).text = "Buinding Text";
-
-        
         GameObject go = GetImage((int)Images.TestImage).gameObject;
         AddUIEvent(go, (PointerEventData data) => { go.transform.position = data.position; }, Define.UIEvent.Drag);
-        
         GetButton((int)Buttons.PointButton).gameObject.AddUIEvent(OnButtonClicked);
-        
     }
+    
     public void OnButtonClicked(PointerEventData data)
     {
         score++;
